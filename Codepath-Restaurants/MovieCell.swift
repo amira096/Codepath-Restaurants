17af963d2036ddc0b08d67be31bad16d0560bef6
amira096 <amira096@fiu.edu>
2021-09-16 20:25:47 -0400

    part 2

---
+++ Codepath-Restaurants/MovieCell.swift
@@ -0,0 +1,28 @@
+//
+//  MovieCell.swift
+//  Codepath-Restaurants
+//
+//  Created by Alejandro Miranda on 9/10/21.
+//  Copyright © 2021 Alejandro Miranda. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Codepath-Restaurants/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  Codepath-Restaurants
+//
+//  Created by Alejandro Miranda on 9/15/21.
+//  Copyright © 2021 Alejandro Miranda. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
